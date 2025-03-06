310f6db381eb306ace536c18026244bbecd3662d
Ryan Hughes <rphughes74@gmail.com>
2025-03-06 00:46:11 -0500

    Transformed data into a filtered down dataset of just the cases we are interested in. Created a linear model for each type of social infrastructure provided within NANDA.
    Troubleshooting warnings within the multilevel model; they may be due to some kind of scaling as there are only very small variations within risk_score between rows and the social infrastructure count appears to have a far smaller effect compared to geography on the risk_score

--- CC_Effects_on_Infrastructure/Join_Datasets.R
+++ CC_Effects_on_Infrastructure/Join_Datasets.R
@@ -3,12 +3,14 @@ library(sf)
 library(tigris)
 library(stringr)
 library(tidyverse)
+library(broom.mixed)
 library(readr)
 library(dplyr)
 library(lme4)
 library(MuMIn)
 #library(nlme)
 
+setwd("CC_Effects_on_Infrastructure")
 load("LoadedData.RData")
 
 #Joining by the X-Y Coords-----------------------------------------------------
@@ -28,23 +30,71 @@ load("LoadedData.RData")
 
 colnames(NANDA_Data)[1]<-"TRACTFIPS"
 
-#Joining the two datasets based on the GEOID value
-Combined_Dataset <- left_join(NRI_Table_CensusTracts, NANDA_Data, by = "TRACTFIPS")
+#First checking for any discrepancies between datasets--------------------------
+Missing=anti_join(NANDA_Data, NRI_Table_CensusTracts, by = "TRACTFIPS")
+max(Missing$totpop,na.rm=TRUE)
+Missing= Missing %>% pivot_longer(cols=c(count_museums, count_theatricalproductions, count_amusementparks,count_movietheaters,count_zoosaquariumsgardens,count_bingocardsgambling,count_poolhallsbowlingalleys,count_totartsentertainment,count_hotels,count_casinohotels),names_to="type",values_to="Infrastructure_Count")
+max(Missing$Infrastructure_Count,na.rm=TRUE)
+print("It appears that the only discrepancies are for uninhabited census tracts which hold no data")
 
+#Filtering out uninhabited census tracts----------------------------------------
+Combined_Dataset <- semi_join(NANDA_Data, NRI_Table_CensusTracts, by = "TRACTFIPS")
+Combined_Dataset <- full_join(Combined_Dataset, NRI_Table_CensusTracts, by = "TRACTFIPS")
+
+#Filtering out Irrelevant Cases-------------------------------------------------
+Filtered_Dataset = Combined_Dataset %>% 
+  select(year,RISK_SCORE, STATE, COUNTY, STCOFIPS, TRACTFIPS, count_museums, count_theatricalproductions, count_amusementparks,count_movietheaters,count_zoosaquariumsgardens,count_bingocardsgambling,count_poolhallsbowlingalleys,count_totartsentertainment,count_hotels,count_casinohotels) %>% 
+  pivot_longer(cols=c(count_museums, count_theatricalproductions, count_amusementparks,count_movietheaters,count_zoosaquariumsgardens,count_bingocardsgambling,count_poolhallsbowlingalleys,count_totartsentertainment,count_hotels,count_casinohotels),names_to="type",values_to="Infrastructure_Count")
+
+
+#Filtering down to Cities of Interest-------------------------------------------------------------
+colnames(all_tracts)[4]<-"TRACTFIPS"
+all_tracts_temp= all_tracts %>% select(city,TRACTFIPS)
+
+all_tracts_temp=st_drop_geometry(all_tracts_temp)
+
+Filtered_Dataset <- inner_join(Filtered_Dataset,all_tracts_temp, by = "TRACTFIPS")
+
+#Evaluating the Variability per Type of Infrastructure--------------------------
+stats= Filtered_Dataset %>%
+  group_by(type,year,STATE, COUNTY,city) %>% 
+  #Note the NANDA dataset holds information from 1990-2021 whilst the NRI scores only capture the most
+  #recent risk scores from Mar-2023
+  summarise(
+    mean_Infra_Count = mean(Infrastructure_Count, na.rm = TRUE),
+    sd_Infra_Count = sd(Infrastructure_Count, na.rm = TRUE),
+    mean_RISK_SCORE = mean(RISK_SCORE, na.rm = TRUE),
+    sd_RISK_SCORE = sd(RISK_SCORE, na.rm = TRUE)
+  )
 #Analyzing Different Simulations!----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#Creating a linear model to determine the correlation between Museum Count (as an Example) against Risk Index
-Linear_Model <- lm(count_museums ~ RISK_SCORE + RISK_VALUE + RISK_SCORE * RISK_VALUE, data = Combined_Dataset)
-
-#Linear Model Assessing All 'Count' Outputs at once
-Linear_Model <- lm(cbind(count_museums, count_theatricalproductions, count_amusementparks,count_movietheaters,count_zoosaquariumsgardens,count_bingocardsgambling,count_poolhallsbowlingalleys,count_totartsentertainment,count_hotels,count_casinohotels) ~ RISK_SCORE + RISK_VALUE + RISK_SCORE * RISK_VALUE, data = Combined_Dataset)
-
+#Seeing if the count of a specific kind of Infrastructure influences the RISK_SCORE and by how much?
+Linear_Model = Filtered_Dataset %>% 
+  group_by(year, type) %>% 
+  reframe(lm(formula= RISK_SCORE ~ Infrastructure_Count, data=. ) %>% broom::tidy())
+print("Notably results do not appear to vary much by year as the risk score is only from 2023")
 # Summary of the model
 summary(Linear_Model)
 
-#Creating a Multilevel Model Grouping by State
-MLM <- lmer(count_museums ~ RISK_SCORE + RISK_VALUE + RISK_SCORE * RISK_VALUE + (1 | STATE), data = Combined_Dataset)
+
+#Creating a Multilevel Model, per Type of Infrastructure, Grouping by State, Country and Tract
+Filtered_Dataset$Risk_Score_Scaled = scale(Filtered_Dataset$RISK_SCORE)
+MLM= Filtered_Dataset %>%
+  group_by(type) %>%
+  group_modify(~ {
+    # Fit the mixed-effects model for each group
+    model <- lmer(Risk_Score_Scaled ~ Infrastructure_Count_Scaled + (1 |TRACTFIPS), data = .x)
+    
+    # Extract the tidy results for fixed effects
+    broom.mixed::tidy(model)})
+#Notably this analysis produces a lot of warnings. This may be due to the fact the Infrastructure count has a very small effect on the Risk_score whilst the TRACTFIPS has a much larger one
 summary(MLM)
+
+#Examining the Correlation between Variables as a check:
+summary(lm(formula=Filtered_Dataset$TRACTFIPS ~ Filtered_Dataset$RISK_SCORE, data=Filtered_Dataset))
+summary(lm(formula=Filtered_Dataset$TRACTFIPS ~ Filtered_Dataset$Infrastructure_Count, data=Filtered_Dataset))
+summary(lm(formula=Filtered_Dataset$Infrastructure_Count ~ Filtered_Dataset$RISK_SCORE, data=Filtered_Dataset))
+print("TRACTFIPS Appears to have the biggest effect on the Infrastructure Count and Risk Score.")
 
 #Creating a Multilevel Model Grouping by State
 MLM <- lmer(count_museums ~ RISK_SCORE + RISK_VALUE + RISK_SCORE * RISK_VALUE + (1 | COUNTY), data = Combined_Dataset)
